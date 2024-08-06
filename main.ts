49f7fb47c703dfc279f5c18d5ffabc3e9ff79278
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2024-08-06 08:54:05 -0400

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,40 @@
+enum Field {
+    //% block="Field 1"
+    Field1 = 1,
+    //% block="Field 2"
+    Field2 = 2,
+    //% block="Field 3"
+    Field3 = 3,
+    //% block="Field 4"
+    Field4 = 4,
+    //% block="Field 5"
+    Field5 = 5
+}
+
+//% color=#0fbc11 icon="\uf1eb"
+namespace ThingSpeak {
+    let apiKey = "";
+    let channelID = 0;
+
+    //% block
+    export function connect(channel: number, key: string): void {
+        channelID = channel;
+        apiKey = key;
+    }
+
+    //% block
+    //% fieldId.defl=Field.Field1
+    export function readData(fieldId: Field): void {
+        control.inBackground(() => {
+            let url = `https://api.thingspeak.com/channels/${channelID}/fields/${fieldId}.json?api_key=${apiKey}&results=1`;
+            let buffer = pins.createBuffer(url.length);
+            for (let i = 0; i < url.length; i++) {
+                buffer.setNumber(NumberFormat.UInt8LE, i, url.charCodeAt(i));
+            }
+            pins.i2cWriteBuffer(0x42, buffer);
+            // Process the response
+        });
+    }
+}
+
+
