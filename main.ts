4bf8c2a9dcfeab4ea1a7c978bea1a31ba6458b83
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2024-08-06 09:41:50 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -18,15 +18,24 @@ namespace ThingSpeak {
     let channelID = 0;
     let fieldValues: number[] = [];
 
-    //% block
+    /**
+     * Connect to a ThingSpeak channel.
+     * @param channel The ThingSpeak channel ID, eg: 12345
+     * @param readKey The read API key for the ThingSpeak channel
+     * @param writeKey The write API key for the ThingSpeak channel
+     */
+    //% block="connect to channel $channel with read key $readKey and write key $writeKey"
     export function connect(channel: number, readKey: string, writeKey: string): void {
         channelID = channel;
         readApiKey = readKey;
         writeApiKey = writeKey;
     }
 
-    //% block
-    //% fieldId.defl=Field.Field1
+    /**
+     * Read data from a specific field in the ThingSpeak channel.
+     * @param fieldId The field number to read from, eg: Field.Field1
+     */
+    //% block="read data from $fieldId"
     export function readData(fieldId: Field): void {
         control.inBackground(() => {
             let url = `https://api.thingspeak.com/channels/${channelID}/fields/${fieldId}.json?api_key=${readApiKey}&results=1`;
@@ -36,15 +45,21 @@ namespace ThingSpeak {
         });
     }
 
-    //% block
-    //% fieldId.defl=Field.Field1
+    /**
+     * Get the value read from a specific field.
+     * @param fieldId The field number to get the value from, eg: Field.Field1
+     */
+    //% block="get value from $fieldId"
     export function getFieldValue(fieldId: Field): number {
         return fieldValues[fieldId];
     }
 
-    //% block
-    //% fieldId.defl=Field.Field1
-    //% value.defl=0
+    /**
+     * Write data to a specific field in the ThingSpeak channel.
+     * @param fieldId The field number to write to, eg: Field.Field1
+     * @param value The value to write to the field, eg: 42
+     */
+    //% block="write $value to $fieldId"
     export function writeData(fieldId: Field, value: number): void {
         control.inBackground(() => {
             let url = `https://api.thingspeak.com/update?api_key=${writeApiKey}&field${fieldId}=${value}`;
